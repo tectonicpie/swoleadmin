7f2ba2475503e0d6d7e26ee2489bd825ea6901f1
tectonicpie <findmuizz@hotmail.com>
2017-04-19 02:01:54 +0500

    added stuff
    Signed-off-by: tectonicpie <findmuizz@hotmail.com>

--- action_page.php
+++ action_page.php
@@ -1,6 +1,20 @@
 <html>
 <body>
 
+[default] 
+aws_access_key_id = AWS access key ID goes here 
+aws_secret_access_key = Secret key goes here         
+
+$sdk = new Aws\Sdk([
+    'region'   => 'us-west-2', // US West (Oregon) Region
+    'version'  => 'latest'  // Use the latest version of the AWS SDK for PHP
+]);
+
+// Create a new DynamoDB client
+$dynamodb = $sdk->createDynamoDb();
+
+
+
 Welcome <?php echo $_POST["uname"]; ?><br>
 Your password is: <?php echo $_POST["psw"]; ?>
 
